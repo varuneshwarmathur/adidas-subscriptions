f21489e410fe16c7f7bdf1a739a8f1933cac21c6
Varuneshwar Nath Mathur <vmathur4@sapient.com>
2019-01-20 04:15:01 +0000

    docker compose added

--- subscriptionservice/src/main/java/com/adidas/subscription/SubscriptionServiceApplication.java
+++ subscriptionservice/src/main/java/com/adidas/subscription/SubscriptionServiceApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 /**
  * @author  varmathu0
  */
 @EnableCircuitBreaker
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableEurekaClient
 public class SubscriptionServiceApplication {
 
     public static void main(String[] args) {
