f208d9e3130662be0e1e4c8b72bd8022c6e8e2bb
Varuneshwar Nath Mathur <vmathur4@sapient.com>
2019-01-20 02:31:14 +0000

    refreshed

--- subscriptionservice/src/test/java/com/adidas/subscription/service/SubscriptionServiceTest.java
+++ subscriptionservice/src/test/java/com/adidas/subscription/service/SubscriptionServiceTest.java
@@ -1,6 +1,7 @@
 package com.adidas.subscription.service;
 
 
+import com.adidas.subscription.model.SubscriptionRequestModel;
 import com.adidas.subscription.model.SubscriptionServiceData;
 import com.adidas.subscription.model.SubscriptionServiceResponse;
 import com.adidas.subscription.repository.SubscriptionServiceDAO;
@@ -8,8 +9,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.dao.DuplicateKeyException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,9 +35,12 @@ public class SubscriptionServiceTest {
     @Test
     public void test_createSubscription() {
         SubscriptionServiceData subscriptionServiceData = mock(SubscriptionServiceData.class);
-        when(subscriptionServiceData.getEmailId()).thenReturn("");
-        when(subscriptionServiceDAO.save(subscriptionServiceData)).thenThrow(DuplicateKeyException.class);
-        SubscriptionServiceResponse subscriptionServiceResponse = subscriptionProcessorService.createSubscription(subscriptionServiceData);
-        }
+        when(subscriptionServiceData.getId()).thenReturn("12321321321312");
+        SubscriptionServiceResponse response = mock(SubscriptionServiceResponse.class);
+        when(subscriptionProcessorService.createSubscription(Mockito.any())).thenReturn(response);
+        SubscriptionServiceResponse subscriptionServiceResponse =
+                subscriptionProcessorService.createSubscription(subscriptionServiceData);
+        assertEquals(response,subscriptionServiceResponse);
+    }
 
 }
