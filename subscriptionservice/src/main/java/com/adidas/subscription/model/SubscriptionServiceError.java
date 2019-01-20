107bd8e0e56fc5f53f0748ac0f549c6966bba4a2
Varuneshwar Nath Mathur <vmathur4@sapient.com>
2019-01-20 00:19:37 +0000

    folders replaced

---
+++ subscriptionservice/src/main/java/com/adidas/subscription/model/SubscriptionServiceError.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.adidas.subscription.model;
+/**
+ * @author  varmathu0
+ */
+public class SubscriptionServiceError {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
